c2c83f4c5dc9d96d17eda069367e79e8efacbb92
Luis Pierro <luisf.pierro@gmail.com>
2020-11-25 13:43:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gama Academy</title>
+    <link rel="stylesheet" href="..\css\biblioteca_estilos.css">
+</head>
+<body>
+    <table border="0" width="100%" cellpadding="10" cellspacing="5" class="menu">
+        <tr>
+            <td align="center"><a href=..\html\index.html><img src="..\imagens\gama.png"></a></td>
+            <td align="center">QUEM SOMOS</td>
+            <td align="center">CURSOS ONLINE</td>
+            <td align="center">GAMA EXPERIENCE</td>
+            <td align="center">PARA EMPRESAS</td>
+            <td align="center"><a href=..\html\contato.html>CONTATO</a></td>        
+            <td align="center">CONTEÚDO</td>
+        </tr>
+    </table>
+    <table border="0" cellpadding="10" cellspacing="5" width="100%">
+        <tr>
+            <td>
+                <img src="..\imagens\imagem1.png">
+            </td>
+            <td>
+                <p>As escolas tradicionais trabalham com metodologias que não conseguem 
+                    suprir as novas demandas do mundo. Preparam profissionais que não são 
+                    capazes de assumir vagas de emprego no mercado atual e, dificilmente, 
+                    lidar com os avanços tecnológicos. Não falam sobre soft skills, habilidades 
+                    altamente requisitadas. E não abordam a importância da multidisciplinaridade.</p>
+                <p>A Gama Academy forma profissionais com quatro características fundamentais:</p>   
+                <ul type="square">
+                    <li>1. Vontade de Aprender e Ensinar</li>
+                    <li>2. Soft Skills</li>
+                    <li>3. Mindset Empreendedor</li>
+                    <li>4. Inovação Criativa</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <div align="center">
+        <h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
 
 <h5>Mudando agora com o pessoal da turma</h5>
 <h4>
@@ -9,3 +51,8 @@
   echo " O resultado da variável feita em PHP é: $i";
   ?>
 </h4>
+    </div>
+</body>
+</html>
+
+
